16aaf67b17759dcafd2d688f4dc8882d2c7b0ff0
Coleman Ferrell <153125444+colemanferrell2@users.noreply.github.com>
2025-06-19 05:22:06 -0400

    Update get_new_results.py

--- data_process_scripts/get_new_results.py
+++ data_process_scripts/get_new_results.py
@@ -86,7 +86,7 @@ def collect_initial_data():
                     parsed_date = datetime.strptime(date_text, "%m/%d").replace(year=current_date.year).date()
                     print(f"🗓 Found meet date: {parsed_date} (delta = {(parsed_date - current_date).days} days)")
         
-                    if abs((parsed_date - current_date).days) > 20:
+                    if abs((parsed_date - current_date).days) > 25:
                        # if abs((parsed_date - current_date).days) <= 20 or abs((parsed_date - current_date).days) >= 45:
                         continue  # ❌ Skip meet too far from today
         
